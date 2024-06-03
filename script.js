d48447b3ef712633cc0130389065f73ea5ebcec8
Ambika Rama Katyayini <35074062+AmbikaKachetti@users.noreply.github.com>
2024-06-03 17:49:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function btnRed() {
+    document.body.style.backgroundColor = "Red";
+    document.getElementById('myH1').style.color = "White";
+}
+function btnBlue() {
+    document.body.style.backgroundColor = "Blue";
+    document.getElementById('myH1').style.color = "White";
+}
+function btnGreen() {
+    document.body.style.backgroundColor = "Green";
+    document.getElementById('myH1').style.color = "White";
+}
+function btnOrange() {
+    document.body.style.backgroundColor = "Orange";
+    document.getElementById('myH1').style.color = "White";
+}
+function btnSkyBlue() {
+    document.body.style.backgroundColor = "Skyblue";
+    document.getElementById('myH1').style.color = "Black";
+}
